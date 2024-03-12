5dff6f96e2e793b19c8a019222e207e73b21d5ff
Suchandra Thapa <sthapa@ci.uchicago.edu>
2024-03-11 19:44:35 -1000

    Update backend start script and docker compose file

--- run-backend.sh
+++ run-backend.sh
@@ -4,4 +4,13 @@ export GDAL_LIBRARY_PATH='/usr/lib/libgdal.so.34'
 export GEOS_LIBRARY_PATH='/usr/lib/libgeos_c.so.1'
 
 cp froide/local_settings.py.example froide/local_settings.py
+
+# To initialise the database:
+/root/.local/bin/poetry run manage.py migrate --skip-checks
+# Create a superuser
+/root/.local/bin/poetry run manage.py createsuperuser
+# Create and populate search index
+/root/.local/bin/poetry run manage.py search_index --create
+/root/.local/bin/poetry run manage.py search_index --populate
+
 /root/.local/bin/poetry run ./manage.py runserver
